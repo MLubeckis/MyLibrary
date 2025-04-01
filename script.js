1247844bc1b4a455397f049cbb425ed589ce7739
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-04-01 13:27:32 +0300

    Add delete and change status buttons

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function Book(title, author, pages, read){
     }
 }
 
+Book.prototype.changeReadStatus = function() {
+    this.read = !this.read;
+    displayBooks();
+}
+
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read)
@@ -18,20 +23,6 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-
-document.querySelector("#new-book-form input[type=button]").addEventListener("click", ()=>{
-    const form = document.querySelector("form");
-    const title = form.title.value
-    const author = form.author.value
-    const pages = form.pages.value
-    const read = form.read.value == 'true' ? true : false;
-    let newBook = new Book(title, author, pages, read)
-    myLibrary.push(newBook);
-    displayBooks()
-    closeModal();
-})
-
-document.querySelector("#openModal").addEventListener("click", openModal);
 
 //#region AddStaticBooks
 addBookToLibrary("ONYX STORM", "Rebecca Yarros", 544, false);
@@ -47,27 +38,50 @@ function displayBooks() {
     booksContent.innerHTML = '';
     const booksTable = document.createElement("table");
     booksTable.classList.add("minimalistBlack");
-    booksTable.innerHTML = "<tr><th>Nosaukums</th><th>Autors</th><th>Lappuses</th><th>Izlasita</th></tr>"
+    booksTable.innerHTML = "<tr><th>Nosaukums</th><th>Autors</th><th>Lappuses</th><th>Izlasita</th><th>Darbibas</th></tr>"
     for (const book of myLibrary) {
         const newRow = document.createElement("tr");
         const tdTitle = document.createElement("td");
         const tdAuthor = document.createElement("td");
         const tdPages = document.createElement("td");
         const tdRead = document.createElement("td");
+        const tdActions = document.createElement("td");
+        const tdDeleteBtn = document.createElement("input");
+        const tdChangeStatusBtn = document.createElement("input");
 
         tdTitle.textContent = book.title;
         tdAuthor.textContent = book.author;
         tdPages.textContent = book.pages;
         tdRead.textContent = book.read == true ? "Ja" : "Ne";
+        tdDeleteBtn.setAttribute("type", "button");
+        tdChangeStatusBtn.setAttribute("type", "button");
+        tdDeleteBtn.setAttribute("value", "Dzest");
+        tdChangeStatusBtn.setAttribute("value", "Mainit statusu");
+        tdDeleteBtn.setAttribute("data-book-id", book.id);
+        tdChangeStatusBtn.setAttribute("data-book-id", book.id);
+
+        tdActions.appendChild(tdDeleteBtn);
+        tdActions.appendChild(tdChangeStatusBtn);
 
         newRow.appendChild(tdTitle);
         newRow.appendChild(tdAuthor);
         newRow.appendChild(tdPages);
         newRow.appendChild(tdRead);
+        newRow.appendChild(tdActions);
         booksTable.appendChild(newRow);
         }
         booksContent.appendChild(booksTable);
-        
+        document.querySelector("table").addEventListener("click",function(evt) {
+            if (evt.target.dataset.bookId != undefined){
+                const book = myLibrary.find(b => b.id == evt.target.dataset.bookId);
+                if (evt.target.value == 'Mainit statusu'){
+                    book.changeReadStatus(); 
+                } else {
+                    myLibrary.splice(myLibrary.findIndex(b => b.id == evt.target.dataset.bookId),1);
+                    displayBooks()
+                }
+            }
+        })
 }
 
 function openModal(){
@@ -82,7 +96,23 @@ function closeModal(){
 displayBooks();
 
 
+//#region EventListeners
+document.querySelector("#new-book-form input[type=button]").addEventListener("click", ()=>{
+    const form = document.querySelector("form");
+    const title = form.title.value
+    const author = form.author.value
+    const pages = form.pages.value
+    const read = form.read.value == 'true' ? true : false;
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook);
+    displayBooks()
+    closeModal();
+})
 
+document.querySelector("#openModal").addEventListener("click", openModal);
+
+
+//#endregion
 
 
 
